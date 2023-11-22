73db3d12bb8dd8dd9ae95af7ec8774fe6a2f8746
hkeena98 <hkeena98@gmail.com>
2023-11-22 01:39:03 -0500

    Starting Instagram Bot Implementation

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # instavid_bot
 Bot for detecting and sharing newly posted videos on Instagram via Messenger.
+
+## Dependencies & Libraries
+- [ensta](https://github.com/diezo/Ensta)
+- [instagrapi](https://github.com/subzeroid/instagrapi)
---
+++ instavid_bot.py
@@ -0,0 +1,41 @@
+"""
+Author: Henry Keena
+License: MIT
+Date: 11/22/23
+Version: 0.1
+"""
+
+# Imports Ensta Library
+#from ensta import Host
+
+# Imports instagrapi Library
+from instagrapi import Client
+
+
+
+"""
+"""
+def test_ensta():
+    print("Testing Ensta Library...\n")
+
+
+
+"""
+"""
+def test_instagrapi():
+    print("Testing instagrapi Library...\n")
+
+
+
+"""
+"""
+def main():
+    print("INSTAGRAM VIDEO BOT")
+    print("Calling Main...\n\n")
+    
+    
+
+
+# Calls Main
+if __name__ == "__main__":
+    main()
