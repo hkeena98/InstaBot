c3cb356abae3b500c5e033ea26b31f95ccab3a26
hkeena98 <hkeena98@gmail.com>
2023-11-25 19:18:30 -0500

    Testing Changes

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 Bot for detecting and sharing newly posted videos on Instagram via Messenger.
 
 ## Dependencies & Libraries
-- [ensta](https://github.com/diezo/Ensta)
+- [Ensta](https://github.com/diezo/Ensta)
 - [instagrapi](https://github.com/subzeroid/instagrapi)
--- instavid_bot.py
+++ instavid_bot.py
@@ -12,6 +12,9 @@ Version: 0.1
 from instagrapi import Client
 
 
+import json
+
+
 
 """
 """
@@ -24,15 +27,37 @@ def test_ensta():
 """
 def test_instagrapi():
     print("Testing instagrapi Library...\n")
+    cl = Client()
+    cl.login("", "")
+    #print("Account Information:",cl.account_info().dict())
+    #print(cl.collections())
+    user_id = cl.user_id_from_username("austinnasso")
+    medias = cl.user_medias(user_id, 20)
+    print(medias)
+    print("Saving to JSON")
+    json_object = json.dumps(medias, default=set_default, indent=4)
+    with open('media_data.json', 'w') as fil:
+        fil.write(json_object)
+    print("\nData Written to JSON File...\n")
+    
+    #for media in medias:
+    #    print(media)
 
+
+# Helper
+def set_default(obj):
+    if isinstance(obj, set):
+        return list(obj)
+    raise TypeError
 
 
 """
 """
 def main():
     print("INSTAGRAM VIDEO BOT")
-    print("Calling Main...\n\n")
-    
+    print("Calling Main...\n")
+    #test_ensta()
+    test_instagrapi()
     
 
 
